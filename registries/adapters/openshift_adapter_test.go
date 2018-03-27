80a64cb638204ece407063e8d3fd2e66e9ec1334
Dylan Murray <dymurray@redhat.com>
2018-03-27 15:39:16 -0400

    Add simple unit tests

---
+++ registries/adapters/openshift_adapter_test.go
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2018 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package adapters
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	ft "github.com/stretchr/testify/assert"
+)
+
+var Images = []string{"satoshi/nakamoto", "foo/bar", "paul/atreides"}
+
+func TestOpenShiftName(t *testing.T) {
+	ocpa := OpenShiftAdapter{}
+	ft.Equal(t, ocpa.RegistryName(), "openshift", "openshift name does not match `openshift`")
+}
+
+func TestOpenShiftGetImageNames(t *testing.T) {
+	ocpa := OpenShiftAdapter{}
+	ocpa.Config.Images = Images
+	imagesFound, err := ocpa.GetImageNames()
+	if err != nil {
+		t.Fatal("Error: ", err)
+	}
+	ft.Equal(t, imagesFound, Images, "image names returned did not match expected config")
+}
