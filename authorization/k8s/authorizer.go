a066e5fb521531f6428f01a5bb45ca7bccf2f64f
Shawn Hurley <smhurley00@gmail.com>
2018-06-21 15:31:52 -0400

    adding tests

--- authorization/k8s/authorizer.go
+++ authorization/k8s/authorizer.go
@@ -7,16 +7,22 @@ import (
 	"github.com/automationbroker/bundle-lib/clients"
 	authv1 "k8s.io/api/authentication/v1"
 	authorizationv1 "k8s.io/api/authorization/v1"
+	"k8s.io/client-go/kubernetes/typed/authorization/v1"
 )
 
 // NewAuthorizer - Create a new authorizer client.
 func NewAuthorizer(group, resource, verb string) (authorization.Authorizer, error) {
+	k, err := clients.Kubernetes()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to connect to the cluster")
+	}
 	return k8sAuthorization{
 		resource: authorizationv1.ResourceAttributes{
 			Group:    group,
 			Resource: resource,
 			Verb:     verb,
 		},
+		client: k.Client.AuthorizationV1().SubjectAccessReviews(),
 	}, nil
 
 }
@@ -33,18 +39,14 @@ func (u AuthorizationUser) Username() string {
 
 type k8sAuthorization struct {
 	resource authorizationv1.ResourceAttributes
+	client   v1.SubjectAccessReviewInterface
 }
 
 func (a k8sAuthorization) Authorize(user authorization.AuthorizeUser, location string) (authorization.Decision, error) {
-	k, err := clients.Kubernetes()
-	if err != nil {
-		return authorization.DecisionDeny, fmt.Errorf("Unable to connect to the cluster")
-	}
 	u, ok := user.(*AuthorizationUser)
 	if !ok {
 		return authorization.DecisionDeny, fmt.Errorf("unknown user structure")
 	}
-
 	r := &a.resource
 	r.Namespace = location
 	sar := &authorizationv1.SubjectAccessReview{
@@ -56,7 +58,7 @@ func (a k8sAuthorization) Authorize(user authorization.AuthorizeUser, location s
 			ResourceAttributes: r,
 		},
 	}
-	sar, err = k.Client.AuthorizationV1().SubjectAccessReviews().Create(sar)
+	sar, err := a.client.Create(sar)
 	if err != nil {
 		return authorization.DecisionDeny, err
 	}
