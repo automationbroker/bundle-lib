87a1ffb763023351f21e26284b6ede5f57f61518
jesus m. rodriguez <jesusr@redhat.com>
2019-07-01 11:47:50 -0400

    Change variable name per review comment

--- registries/adapters/oauth/client.go
+++ registries/adapters/oauth/client.go
@@ -84,13 +84,13 @@ func (c *Client) NewRequest(path string) (*http.Request, error) {
 
 	// path might be a fully qualified URL and we only want the path
 	// and query bits
-	thebits, err := url.Parse(path)
+	pathAsUrl, err := url.Parse(path)
 	if err != nil {
 		return nil, err
 	}
 
-	req.URL.Path = thebits.Path
-	req.URL.RawQuery = thebits.Query().Encode()
+	req.URL.Path = pathAsUrl.Path
+	req.URL.RawQuery = pathAsUrl.Query().Encode()
 	if c.token != "" {
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
 	}
